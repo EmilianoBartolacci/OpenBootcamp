df3dd454e96bc365d8281ea19771d96b66d24d94
Emiliano Bartolacci <emibarto@live.com.ar>
2022-10-03 14:04:45 -0300

    03/10/2022

---
+++ Python/Ejercicio 5/Ejercicio 5.py
@@ -0,0 +1,13 @@
+# Bartolacci Emiliano
+# Ejercicio 5
+
+def añoBisiesto(año) :
+    if año % 4 == 0:
+        print(año, "es un año bisiesto")
+    else:
+        print(año, "no es un año bisiesto")
+
+añoBisiesto(2020)
+
+# output =
+# 2020 es un año bisiesto
