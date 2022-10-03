a315cb57f59276609f14fd4c20eb8b5e5b908b43
Emiliano Bartolacci <emibarto@live.com.ar>
2022-10-03 19:23:37 -0300

    03/10/2022

---
+++ Python/Ejercicio 7/Ejercicio 7 parte uno.py
@@ -0,0 +1,20 @@
+# Bartolacci Emiliano
+# Ejercicio 7 parte uno
+
+import Calculos
+
+suma = Calculos.suma(3, 5)
+resta = Calculos.resta(8, 2)
+multiplicacion = Calculos.multiplicar(5, 8)
+divicion = Calculos.divicion(8, 4)
+
+print("La suma total es", suma)
+print("La resta total es", resta)
+print("La multiplicacion total es", multiplicacion)
+print("La divicion total es", divicion)
+
+# output =
+# La suma total es 8
+# La resta total es 6
+# La multiplicacion total es 40
+# La divicion total es 2
